b5009e0c5a554ec0f95e424fb71a2d130446e38e
cadermill <145504984+cadermill@users.noreply.github.com>
2025-07-12 13:29:47 -0400

    load_data() implementation
    fixed data type inconsistencies and changed destination path to data folder outside repository

--- main.py
+++ main.py
@@ -1,33 +1,49 @@
+import os
 import pandas as pd
 
-# 1,588,240 rows
-# tconst    averageRating   numVotes
-# tt0000001 5.7             2165
-# str       float64         int64
-ratings = pd.read_csv('../data/title.ratings.tsv.gz', sep='\t', compression='gzip', low_memory=False)
+def load_data():
+    if os.path.exists('../data/cleanedTitles.csv'):
+        print("Loading data from cleanedTitles.csv...")
+        return pd.read_csv('../data/cleanedTitles.csv', low_memory=False)
+    else:
+        print("Processing data files...")
+        # 1,588,240 rows
+        # tconst    averageRating   numVotes
+        # tt0000001 5.7             2165
+        # str       float64         int64
+        ratings = pd.read_csv('../data/title.ratings.tsv.gz', sep='\t', compression='gzip', low_memory=False, on_bad_lines='skip')
 
-# 11,769,857 rows
-# tconst    titleType   primaryTitle    originalTitle   isAdult     startYear   endYear runtimeMinutes  genres
-# tt0000001 short       Carmencita      Carmencita      0           1894        \N      1               Documentary,Short
-# str       str         str             str             str         str         str     str                             str
-basics = pd.read_csv('../data/title.basics.tsv.gz', sep='\t', compression='gzip', low_memory=False)
+        # 11,769,857 rows
+        # tconst    titleType   primaryTitle    originalTitle   isAdult     startYear   endYear runtimeMinutes  genres
+        # tt0000001 short       Carmencita      Carmencita      0           1894        \N      1               Documentary,Short
+        # str       str         str             str             str         str         str     str                             str
+        basics = pd.read_csv('../data/title.basics.tsv.gz', sep='\t', compression='gzip', low_memory=False, on_bad_lines='skip')
 
-# 11,771,649 rows
-# tconst     directors    writers
-# tt0000001  nm0005690    \N
-# str        str          str
-crew = pd.read_csv('../data/title.crew.tsv.gz', sep='\t', compression='gzip', low_memory=False)
+        # 11,771,649 rows
+        # tconst     directors    writers
+        # tt0000001  nm0005690    \N
+        # str        str          str
+        crew = pd.read_csv('../data/title.crew.tsv.gz', sep='\t', compression='gzip', low_memory=False, on_bad_lines='skip')
 
-# merge ratings, crew, and basics on 'tconst'
-titles = pd.merge(ratings, crew, on='tconst', how='left')
-titles = pd.merge(titles, basics, on='tconst', how='left')
+        # merge ratings, crew, and basics on 'tconst'
+        titles = pd.merge(ratings, crew, on='tconst', how='left')
+        titles = pd.merge(titles, basics, on='tconst', how='left')
 
-# selcect only the columns we need
-titles = titles[['tconst', 'averageRating', 'numVotes', 'directors', 'writers', 'titleType', 'primaryTitle', 'startYear', 'runtimeMinutes', 'genres']]
+        # selcect only the columns we need
+        titles = titles[['tconst', 'averageRating', 'numVotes', 'directors', 'writers', 'titleType', 'primaryTitle', 'startYear', 'runtimeMinutes', 'genres']]
 
-# convert \N to NA
-titles.replace('\\N', pd.NA, inplace=True)
+        # convert \N to NA
+        titles.replace('\\N', pd.NA, inplace=True)
 
-print(titles.shape)
-print(titles.head())
-titles.to_csv('cleanedTitles.csv')
+        # convert to appropriate data types
+        titles['averageRating'] = pd.to_numeric(titles['averageRating'], errors='coerce')
+        titles['numVotes'] = pd.to_numeric(titles['numVotes'], errors='coerce')
+        titles['startYear'] = pd.to_numeric(titles['startYear'], errors='coerce')
+        titles['runtimeMinutes'] = pd.to_numeric(titles['runtimeMinutes'], errors='coerce')
+
+        # save the cleaned data
+        titles.to_csv('../data/cleanedTitles.csv', index=False)
+
+        return titles
+
+titles = load_data()
